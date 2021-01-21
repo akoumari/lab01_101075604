939ac4e5d6a07d5c36abcb3606b62d27ae818ff8
akoumari <alex.koumarianos@gmail.com>
2021-01-20 19:20:46 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lab01_101075604
+!["Screenshot"](screenshot.PNG)
---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require("fs")
+const csv = require("csv-parser")
+let countries =[]
+let canada = []
+let usa = []
+
+const getSingleCountry = (countryName, dataSet) =>{
+  return  dataSet.filter(line=>line.country.toLowerCase() == countryName.toLowerCase() )
+}
+const makeContent = (dataSet) => {
+    let content = "country,year,population\n"
+    dataSet.map(rec=>{
+        let {country,year,population} = rec
+        content += `${country},${year},${population}\n`
+    })
+    return content
+}
+fs.createReadStream("input_countries.csv")
+  .pipe(csv())
+  .on('data', (data) => countries.push(data))
+  .on('end', () => {
+    console.log("parsing completed... YISSSS!")
+     canada = getSingleCountry("canada",countries)
+     usa = getSingleCountry("united States",countries)
+     fs.readdirSync(".",(err, listOfFiles)=>{
+        listOfFiles.filter(file=>{file=="canada.txt"||file=="usa.txt"}).map(file=>{
+            fs.unlink(file,(err)=>{
+                if(err){
+                    console.log(`whoops big boi we got an error with file: ${file}`)
+                    console.log(err)
+                }
+            })
+        })
+     })
+     fs.writeFile("canada.txt", makeContent(canada), (err)=>{
+            console.log("Write Successful")
+        })
+     fs.writeFile("usa.txt", makeContent(usa), (err)=>{
+            console.log("Write Successful")
+        })
+        console.log("The beacons has been lit sir. This must mean the dead was done")
+
+    });
+
+  
